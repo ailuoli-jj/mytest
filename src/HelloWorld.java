b09ff44a53458a5a92f569d3e1bdd63fb83438fd
Administrator <Administrator@PC201902121648C>
2019-08-24 09:08:42 +0800

    第一次提交

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld !!!!!");
+	    	System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nsay,hello");
+ 	}
+
+
+}
